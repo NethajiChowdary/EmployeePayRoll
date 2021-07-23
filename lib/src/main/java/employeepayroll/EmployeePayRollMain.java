13b57b3f1f63fffa545679fb6471bdbf24909132
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-24 00:31:47 +0530

    Initial Commit: Welcome to Employee Pay Roll Service

---
+++ lib/src/main/java/employeepayroll/EmployeePayRollMain.java
@@ -0,0 +1,10 @@
+package employeepayroll;
+
+public class EmployeePayRollMain {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Employee Pay Roll Service");
+	}
+
+}
