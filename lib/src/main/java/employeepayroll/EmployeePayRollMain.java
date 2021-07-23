dbce0ae6efd1471f515ea9b3f355b96dd21609cd
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-24 00:53:50 +0530

    Code to Watch Particular Directory Along With all Files

--- lib/src/main/java/employeepayroll/EmployeePayRollMain.java
+++ lib/src/main/java/employeepayroll/EmployeePayRollMain.java
@@ -5,7 +5,7 @@ public class EmployeePayRollMain
 	public static void main(String[] args) 
 	{
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-		System.out.println("Welcolme to Employee PayRoll");
+		System.out.println("Welcolme to employee payRoll");
 		employeePayrollService.readEmployeeDataFromConsole();
 		employeePayrollService.writeEmployeeDataInConsole();
 	}
